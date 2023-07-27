4a83eaa535bece14e7ab2ae22d024b315ff2d314
Iuliana Filip <iulianafilip.office@gmail.com>
2023-07-27 15:34:37 +0300

    I had to change are with input line14-15

--- frontend/pages/data_update.py
+++ frontend/pages/data_update.py
@@ -11,8 +11,8 @@ quarters = st.selectbox("Please select quarter",["Q1", "Q2", "Q3"])
 category = st.selectbox("Please select category",["Sales metric calcutation","individual performance tracking", "team performance tracking"])
 subcategory = st.selectbox("Please select subcategory",["Revenue", "Unit sold", "Conversion rate","Sales targets", "Achived sales","Team targets", "Overall revenue"])
 change_made = st.text_input("What do you want to change?")
-report_made = st.text_area("Reason", "Please explain your change.")
-output = st.text_area("Recomandations", "What futher action you recomand.")
+report_made = st.text_input("Reason", "Please explain your change.")
+output = st.text_input("Recomandations", "What futher action you recomand?")
 
 if st.button("Submit"):
     st.write("Update submitted successfully.")
