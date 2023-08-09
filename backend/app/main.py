46207bff09aad00bf5bd26b5029feab09d04cc76
Iuliana Filip <iulianafilip.office@gmail.com>
2023-08-09 16:45:00 +0300

    added sqlmodel, create db,dockerfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,23 @@
 from __future__ import annotations
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException
 from datetime import date
 from enum import Enum
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from pydantic import BaseModel
+
 from typing import Union, Literal # , TypeAlias
 
-app = FastAPI()
+sql_file_name = "database.db"
+sqlite_url = f"sqlite:///{sql_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)    
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 class Status(str, Enum):
@@ -15,7 +26,7 @@ class Status(str, Enum):
     Reviewed = "Reviewed"
     Closed = "Closed"
 
-class SeeReport(BaseModel):
+class SeeReport(SQLModel):
     name: str
     quarters: str
     metric_calculations: str
@@ -25,7 +36,7 @@ class SeeReport(BaseModel):
 
 
       
-class AddChange(BaseModel):
+class AddChange(SQLModel):
     quarters: str
     category: str
     subcategory: str
@@ -34,25 +45,43 @@ class AddChange(BaseModel):
     output: str
     status: Status = Status.Open
     created_at: date = date.today()
-   
-class SalePerformance(SQLModel, SeeReport, AddChange ): 
+
+    class Config:
+        orm_mode = True
+        
+class ChangeCreate(SeeReport, AddChange):
+    pass
+class ChangeRead(SeeReport, AddChange):
+    id: int
+class SalePerformance(SeeReport, AddChange): 
     id: int = Field(default=None, primary_key=True)
+    
     
     class Config:
         orm_mode = True
  
-saleperformance_db = []
+
+
+app = FastAPI() 
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
     
 
-@app.get("/salesperformance")
-async def get_report():
-    return saleperformance_db
+@app.get("/salesperformance", response_model=list[SalePerformance])
+async def get_report(*, session: Session = Depends(get_session)):
+    salesperformance = session.exec(select(ChangeRead)).all()
+    return salesperformance
 
 @app.post("/salesperformance1", response_model=SalePerformance)
-async def create_change(item: AddChange):
-    sale_performance = SalePerformance(**item.dict())
-    saleperformance_db.append(sale_performance)
-    return sale_performance
+async def create_change(*, change: ChangeCreate, session: Session = Depends(get_session)):
+    db_change = SalePerformance(**change.dict())
+    session.add(db_change)
+    session.commit()
+    session.refresh(db_change)
+    return db_change
+    
 
 Quarter = Literal["Q1", "Q2", "Q3", "Q4"]
 Category = Literal["Revenue", "Unit sold", "Conversion rate"]
@@ -61,7 +90,7 @@ Change_made = Literal["Team targets", "Overall revenue", "Conversion rate"]
 Report_made = Literal["Patterns", "Trends", "segment customer"]
 Output = Literal["Patterns", "Trends", "segment customer"]
     
-@app.post("/salesperformance2")
+@app.post("/salesperformance2", response_model=SalePerformance)
 async def create_change(quarters: Quarter, category: Category, subcategory: Subcategory, change_made: Change_made, report_made:Report_made, output:Output):
     change = {
         "quarters": quarters,
@@ -72,3 +101,4 @@ async def create_change(quarters: Quarter, category: Category, subcategory: Subc
         "output": output
     }
     return change
+
