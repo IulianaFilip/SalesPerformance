6022e931e11070714e05624dc5540416e2932717
Iuliana Filip <iulianafilip.office@gmail.com>
2023-08-19 20:07:04 +0300

    updated main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,97 +1,100 @@
 from __future__ import annotations
-from fastapi import FastAPI,Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from datetime import date
 from enum import Enum
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+from pydantic import BaseModel
 
-from typing import Union, Literal # , TypeAlias
+from typing import Union, Literal, List  # , TypeAlias
 
 sql_file_name = "database.db"
 sqlite_url = f"sqlite:///{sql_file_name}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)    
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    
+
+
 def get_session():
     with Session(engine) as session:
         yield session
 
 
 class Status(str, Enum):
-    Open = "Open"
-    Made = "Made"
-    Reviewed = "Reviewed"
-    Closed = "Closed"
+     Open = "Open"
+     InProgress = "InProgress"
+     Closed = "Closed"
+
 
 class SeeReport(SQLModel):
-    name: str
-    quarters: str
-    metric_calculations: str
-    individual_performance: str
-    team_performance: str
-    customer_behavior: str
+     name: str
+     quarters: str
+     metric_calculations: str
+     individual_performance: str
+     team_performance: str
+     customer_behavior: str
 
 
-      
 class AddChange(SQLModel):
+     quarters: str
+     category: str
+     subcategory: str
+     change_made: str
+     report_made: str
+     output: str
+     status: Status = Status.Open
+     created_at: date = date.today()
+
+
+class ChangeCreate(AddChange):
+     pass
+
+
+class ChangeRead(AddChange):
+    id: int
+
+
+class SalesPerformance(BaseModel):
     quarters: str
     category: str
     subcategory: str
     change_made: str
     report_made: str
     output: str
-    status: Status = Status.Open
-    created_at: date = date.today()
-
-    class Config:
-        orm_mode = True
-        
-class ChangeCreate(SeeReport, AddChange):
-    pass
-class ChangeRead(SeeReport, AddChange):
-    id: int
-class SalePerformance(SeeReport, AddChange): 
-    id: int = Field(default=None, primary_key=True)
-    
-    
-    class Config:
-        orm_mode = True
- 
 
 
-app = FastAPI() 
+class Config:
+     orm_mode = True
+
+
+app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    
 
-@app.get("/salesperformance", response_model=list[SalePerformance])
+
+
+
+@app.get("/salesperformance", response_model=List[SalesPerformance])
 async def get_report(*, session: Session = Depends(get_session)):
-    salesperformance = session.exec(select(ChangeRead)).all()
-    return salesperformance
+    sales_performance = session.exec(select(SalesPerformance)).all()
+    return sales_performance
 
-@app.post("/salesperformance1", response_model=SalePerformance)
-async def create_change(*, change: ChangeCreate, session: Session = Depends(get_session)):
-    db_change = SalePerformance(**change.dict())
-    session.add(db_change)
-    session.commit()
-    session.refresh(db_change)
-    return db_change
-    
 
-Quarter = Literal["Q1", "Q2", "Q3", "Q4"]
-Category = Literal["Revenue", "Unit sold", "Conversion rate"]
-Subcategory = Literal["Sales targets", "Achived sales", "Conversion rate"]
-Change_made = Literal["Team targets", "Overall revenue", "Conversion rate"]
-Report_made = Literal["Patterns", "Trends", "segment customer"]
-Output = Literal["Patterns", "Trends", "segment customer"]
-    
-@app.post("/salesperformance2", response_model=SalePerformance)
-async def create_change(quarters: Quarter, category: Category, subcategory: Subcategory, change_made: Change_made, report_made:Report_made, output:Output):
+
+@app.post("/salesperformance")
+async def sales_performance(sales_data: SalesPerformance):
+    quarters = sales_data.quarters
+    category = sales_data.category
+    subcategory = sales_data.subcategory
+    change_made = sales_data.change_made
+    report_made = sales_data.report_made
+    output = sales_data.output
+   
     change = {
         "quarters": quarters,
         "category": category,
@@ -99,6 +102,7 @@ async def create_change(quarters: Quarter, category: Category, subcategory: Subc
         "change_made": change_made,
         "report_made": report_made,
         "output": output
+        
     }
     return change
 
